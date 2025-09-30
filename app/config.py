59f08978282c0acc92157321c34d10c42eb74dcb
Michael <estorque.michael@student.capsu.edu.ph>
2025-09-30 18:57:43 +0800

    database name change to atbi_db

--- app/config.py
+++ app/config.py
@@ -5,9 +5,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev_secret_key")
 
     # PostgreSQL connection URL
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "postgresql+psycopg2://postgres:cyla0917@localhost:5432/atbiDB")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql+psycopg2://postgres:cyla0917@localhost:5432/atbi_db")
 
     # Disable track modifications to save memory
     SQLALCHEMY_TRACK_MODIFICATIONS = False
